34835480c828063b61abab19cc848a7c756e901c
AntonVladimir <a.b.v@bk.ru>
2016-09-01 21:42:20 +0400

    обработка required

--- script/form-required.js
+++ script/form-required.js
@@ -1,28 +1,58 @@
 console.log("i am hire");
 
 var name_form = document.forms.form_footer;
-var required_text_id = document.getElementById("required_text_id");
-var required_email_id = document.getElementById("required_email_id");
+var required_text = document.getElementsByClassName("required_text")[0];
+var required_email = document.getElementsByClassName("required_email")[0];
 
-console.log(required_text_id+" "+required_text_id.className);
-console.log(required_email_id+" "+required_email_id.className);
+console.log(required_text+" "+required_text.className);
+console.log(required_email+" "+required_email.className);
 
 console.log(name_form.name.type);
 console.log(name_form.email.type);
+
+// ----------------------------------------
 
 name_form.name.onblur = function(){
 	// console.log("blur");
 
 	if(this.value == ""){
 		console.log("Пусто бля!!!");
+
+		required_text.style.display = "block";
 		
-		this.style.backgroundColor = "#FF986E";
-		this.style.borderColor = "#ff0000";
 	}else{
 		console.log("Нормально.");
 	}
 }
 	
+name_form.name.onfocus = function(){
+	console.log("this is focus");
+
+	required_text.style.display = "none";
+}
+
+// --------------------------------------------
+
+
+name_form.email.onblur = function(){
+	// console.log("blur");
+
+	if(this.value == ""){
+		console.log("Пусто бля!!!");
+
+		required_email.style.display = "block";
+		
+	}else{
+		console.log("Нормально.");
+	}
+}
+	
+name_form.email.onfocus = function(){
+	console.log("this is focus");
+
+	required_email.style.display = "none";
+}
+
 
 // var email = document.querySelector("#email");
 
